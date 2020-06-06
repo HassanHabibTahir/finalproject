d5b5e64ef343ad95d6ee79ba112d6c5ac1b5360c
Hassan Habib Tahir <hassanhabibtahir@gmail.com>
2020-06-06 20:06:17 +0500

    6-2020

--- client/src/components/Mens/women/women.js
+++ client/src/components/Mens/women/women.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react'
 import Card from '@material-ui/core/Card';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import Favorite from '@material-ui/icons/Favorite';
+import Fab from '@material-ui/core/Fab';
+import history from '../../history/history'
+import {Link} from 'react-router-dom';
+import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
  import {getCategoryProduct} from '../../../store/action/products/productaction'
 
  import { connect } from 'react-redux'; 
@@ -14,7 +23,28 @@ import Card from '@material-ui/core/Card';
   
   } 
     
+  addToFav=(fav ,auth)=>{
+    if(!auth.isAuthenticated===false){
+   this.props.FavouritAdds(fav)
+  //  alert(fav)
+    }
+    else{
+      history.push('/login')
+    }
+  }
 
+
+  changedData=(i,img)=>{
+// console.log(i)
+// const index = this.target.valaue
+// console.log(index)
+// .imageContainer.src
+const containerSrc = this.refs[i].src
+  
+console.log("this is second",containerSrc)
+this.refs[i].src="http://localhost:8080/"+img
+// console.log(img) 
+  }
     render() {
 console.log(this.props.Products)
  const  MenProducts = this.props.Products
@@ -25,30 +55,44 @@ console.log(this.props.Products)
   
 })
 console.log(Prdouctsitems)
-      let product= Prdouctsitems.map((item)=>{
-
+      let product= Prdouctsitems.map((item,i)=>{
+        let id=item._id
 return    <Card className="main">
+<Fab size="medium" color="secondary" aria-label="add" >
+{item.price}$
+      </Fab>
+<FormControlLabel
+fontSize="large"
+
+style={{float:"right"}}
+control={<Checkbox onClick = {()=>{this.addToFav(item,this.props.auth)}} checked = {item.fav ? true : false}  icon={<FavoriteBorder fontSize="large"  />} checkedIcon={<Favorite  fontSize="large" />} name="checkedH" />}
+   
+     />
+
+
 
 <div className="card_products">
-  {/* <div className="deleteButton"> <Button className="buttondel" > <DeleteIcon   className="icon_del" /></Button></div> */}
+ {/* <div className="deleteButton"> <Button className="buttondel" > <DeleteIcon   className="icon_del" /></Button></div> */}
 
 <div className="top-section">
+                                                           
 
-  
-   <center> <img  className="img_container" src={"http://localhost:8080/"+item.imgSrc[0]} alt="img1" /></center>
-
+  <center> <Link  to={`productitems/${item._id}`}><img    ref = {id}  valaue={i}    className="img_container" src={"http://localhost:8080/"+item.imgSrc[0]} alt="img1" /></Link></center>
 
 <div className="nav">
 
-<img src={"http://localhost:8080/"+item.imgSrc[1]}/>
-<img src={"http://localhost:8080/"+item.imgSrc[2]}/>
-<img src={"http://localhost:8080/"+item.imgSrc[3]}/>
+<img    onClick={()=>{this.changedData( item._id  ,item.imgSrc[0])}} ref = 'image'  src={"http://localhost:8080/"+item.imgSrc[0]}/>
+<img    onClick={()=>{this.changedData(item._id,item.imgSrc[1])}}    ref = 'image' src={"http://localhost:8080/"+item.imgSrc[1]}/>
+<img    onClick={()=>{this.changedData(item._id,item.imgSrc[2])}}    ref = 'image'  src={"http://localhost:8080/"+item.imgSrc[2]}/>
+<img    onClick={()=>{this.changedData(item._id,item.imgSrc[3])}}    ref = 'image'  src={"http://localhost:8080/"+item.imgSrc[3]}/>
 
 </div>
 
-<div className="product_info">
+<div className="prduct_info">
 
-<div  className="name_product"><h2>{item.productname.toUpperCase()}<span className="price">${item.price}</span></h2>
+<div  className="name_product"><h2>{item.productname.toUpperCase()}
+{/* <span className="price">${item.price}</span> */}
+</h2>
 
 </div>
 <div className="dis">{item.discription}</div>
@@ -59,7 +103,7 @@ return    <Card className="main">
 
 </div>
 
-               </Card>
+              </Card>
  
 })
 
@@ -74,8 +118,8 @@ return    <Card className="main">
 }
 const mapStateToProps=(state)=>({
   
-    Products:state.allProducts.AllusersProducts
-   
+    Products:state.allProducts.AllusersProducts,
+    auth:state.auth,
 
   
   })
