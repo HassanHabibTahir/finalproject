202c2d20beb66f143546fd518baed63c8f0e8519
Hassan Habib Tahir <hassanhabibtahir@gmail.com>
2020-06-07 12:48:39 +0500

    7-2020

--- client/src/components/Mens/mens/men.js
+++ client/src/components/Mens/mens/men.js
@@ -9,6 +9,7 @@ import Fab from '@material-ui/core/Fab';
 import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
  import {getAllMenProduts ,FavouritAdds} from '../../../store/action/products/productaction'
 import history from '../../history/history'
+import MenCard from '../card/card'
  import './men.css'
  import { connect } from 'react-redux'; 
 import {Link} from 'react-router-dom';
@@ -58,53 +59,70 @@ console.log(this.props.Products)
 // console.log(Prdouctsitems)
   let product= Prdouctsitems.map((item,i)=>{
     let id=item._id
-return    <Card className="main">
- <Fab size="medium" color="secondary" aria-label="add" >
-{item.price}$
-       </Fab>
-<FormControlLabel
-fontSize="large"
 
-style={{float:"right"}}
-control={<Checkbox onClick = {()=>{this.addToFav(item,this.props.auth)}} checked = {item.fav ? true : false}  icon={<FavoriteBorder fontSize="large"  />} checkedIcon={<Favorite  fontSize="large" />} name="checkedH" />}
+
+return(
+  <MenCard 
+            
+  images={item.imgSrc}
+  price={item.price}
+  description={item.discription}
+  productName = {item.productName}
+  category={item.category}
+  _id={item._id}
+  id={item._id}
+  i={i}
+  fav={item.fav}
+/>
+)
+
+// return    <Card className="main">
+//  <Fab size="medium" color="secondary" aria-label="add" >
+// {item.price}$
+//        </Fab>
+// <FormControlLabel
+// fontSize="large"
+
+// style={{float:"right"}}
+// control={<Checkbox onClick = {()=>{this.addToFav(item,this.props.auth)}} checked = {item.fav ? true : false}  icon={<FavoriteBorder fontSize="large"  />} checkedIcon={<Favorite  fontSize="large" />} name="checkedH" />}
     
-      />
+//       />
 
 
 
-<div className="card_products">
-  {/* <div className="deleteButton"> <Button className="buttondel" > <DeleteIcon   className="icon_del" /></Button></div> */}
+// <div className="card_products">
+//   {/* <div className="deleteButton"> <Button className="buttondel" > <DeleteIcon   className="icon_del" /></Button></div> */}
 
-<div className="top-section">
+// <div className="top-section">
                                                             
 
-   <center> <Link  to={`productitems/${item._id}`}><img    ref = {id}  valaue={i}    className="img_container" src={"http://localhost:8080/"+item.imgSrc[0]} alt="img1" /></Link></center>
+//    <center> <Link  to={`productitems/${item._id}`}><img    ref = {id}  valaue={i}    className="img_container" src={"http://localhost:8080/"+item.imgSrc[0]} alt="img1" /></Link></center>
 
-<div className="nav">
+// <div className="nav">
 
-<img    onClick={()=>{this.changedData( item._id  ,item.imgSrc[0])}} ref = 'image'  src={"http://localhost:8080/"+item.imgSrc[0]}/>
-<img    onClick={()=>{this.changedData(item._id,item.imgSrc[1])}}    ref = 'image' src={"http://localhost:8080/"+item.imgSrc[1]}/>
-<img    onClick={()=>{this.changedData(item._id,item.imgSrc[2])}}    ref = 'image'  src={"http://localhost:8080/"+item.imgSrc[2]}/>
-<img    onClick={()=>{this.changedData(item._id,item.imgSrc[3])}}    ref = 'image'  src={"http://localhost:8080/"+item.imgSrc[3]}/>
+// <img    onClick={()=>{this.changedData( item._id  ,item.imgSrc[0])}} ref = 'image'  src={"http://localhost:8080/"+item.imgSrc[0]}/>
+// <img    onClick={()=>{this.changedData(item._id,item.imgSrc[1])}}    ref = 'image' src={"http://localhost:8080/"+item.imgSrc[1]}/>
+// <img    onClick={()=>{this.changedData(item._id,item.imgSrc[2])}}    ref = 'image'  src={"http://localhost:8080/"+item.imgSrc[2]}/>
+// <img    onClick={()=>{this.changedData(item._id,item.imgSrc[3])}}    ref = 'image'  src={"http://localhost:8080/"+item.imgSrc[3]}/>
 
-</div>
+// </div>
 
-<div className="prduct_info">
+// <div className="prduct_info">
 
-<div  className="name_product"><h2>{item.productname.toUpperCase()}
-{/* <span className="price">${item.price}</span> */}
-</h2>
+// <div  className="name_product"><h2>{item.productname.toUpperCase()}
+// {/* <span className="price">${item.price}</span> */}
+// </h2>
 
-</div>
-<div className="dis">{item.discription}</div>
+// </div>
+// <div className="dis">{item.discription}</div>
 
-</div>
-</div>
+// </div>
+// </div>
 
 
-</div>
+// </div>
 
-               </Card>
+//                </Card>
  
 })
 
