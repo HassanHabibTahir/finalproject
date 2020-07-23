7fe073070c9835bd8985c7d7ee238b4b498b9f6b
Hassan Habib Tahir <hassanhabibtahir@gmail.com>
2020-07-23 13:13:08 +0500

    july-23-2020

--- src/config/key.js
+++ src/config/key.js
@@ -1,3 +1,5 @@
 module.exports={
-    secretOrkey:'Hassan is BadSha'
+    secretOrkey:'Hassan is BadSha',
+     url :' mongodb://127.0.0.1:27017/GarmentsProduct'
 }
+// const url='mongodb+srv://HassanHabibTahir:golabkhan123@gobachi.9toad.mongodb.net/GOBACHI?retryWrites=true&w=majority'
--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
-// const url='mongodb+srv://HassanHabibTahir:golabkhan123@gobachi.9toad.mongodb.net/GOBACHI?retryWrites=true&w=majority'
-const  url =' mongodb://127.0.0.1:27017/GarmentsProduct'
-mongoose.connect(url,{
+const db = require('../config/key')
+
+
+mongoose.connect(db.url,{
     useNewUrlParser:true,
     useCreateIndex:true,
     useFindAndModify:false
